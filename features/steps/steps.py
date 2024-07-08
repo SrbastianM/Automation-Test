b8b0701ad241aafdcfbe2c9ef024ee5c0b2b6503
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-07-08 13:13:18 -0500

    Configure steps to use another page

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -2,8 +2,8 @@ from behave import given, when, then
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 import json
 
 with open('config.json', 'r') as url:
@@ -14,36 +14,64 @@ def webDriver(context):
     context.driver = webdriver.Firefox()
     context.driver.get(config["url"])
     context.driver.maximize_window()
-    context.driver.execute_script("window.scrollTo(0, 800)")
+    time.sleep(10)
+     
+    try:
+        cookieAlert = context.driver.find_element(By.XPATH, "//button[contains(text(),'Aceptar')]")
+        
+        if cookieAlert.is_displayed() and cookieAlert.is_enabled():
+            try:
+                cookieAlert.click()
+                print("Cookie accepted with selenium click")
+            except:
+                 context.driver.execute_script("arguments[0].click();", cookieAlert)
+                 print("Cookie accepted with selenium click")
+            print("Cookies is accepted")
+    except Exception as e:
+        print("Element Not found or not clickable", str(e))
+        context.driver.quit()
+    
+    
 
 @when('El usuario ingresa sus credenciales')
 def credentials(context):
-    context.driver.find_element(By.ID,"username").send_keys(config["username"])
-    context.driver.find_element(By.ID,"password").send_keys(config["password"])
+    context.driver.implicitly_wait(20)
+    context.driver.find_element(By.ID,"userName").send_keys(config["correctUser"]["username"])
+    context.driver.find_element(By.ID,"password").send_keys(config["correctUser"]["password"])
     
 @when('El usuario ingresa sus credenciales de manera incorrecta')
 def incorrect_credentials(context):
-    context.driver.find_element(By.ID,"username").send_keys("practice incorrect")
-    context.driver.find_element(By.ID,"password").send_keys("SuperSecretPasswordIncorrect!")
+    context.driver.implicitly_wait(20)
+    context.driver.find_element(By.XPATH,"//input[@id='userName']").send_keys(config["incorrectUser"]["username"])
+    context.driver.find_element(By.XPATH,"//input[contains(@type,'password')]").send_keys(config["incorrectUser"]["password"])
     
 @when('El usuario hace clic en el botón ingresar')
 def click_login_button(context):
-    context.driver.find_element(By.XPATH, "//button[@type='submit'][contains(.,'Login')]").click()
+    time.sleep(2)
+    btnLogin = context.driver.find_element(By.XPATH, "//button[contains(text(),'Entrar')]")
+    if btnLogin.is_displayed() and btnLogin.is_enabled():
+        btnLogin.click()
     
 @then('El usuario visualiza el mensaje de credenciales incorrectas')
 def login_fail(context):
-    assert "Your username is invalid!" in context.driver.page_source
+    time.sleep(2)
+    context.driver.find_element(By.XPATH, "//p[@class='first-body'][contains(.,'Usuario o contraseña incorrectos.')]")
+    assert " Usuario o contraseña incorrectos. " in context.driver.page_source
+    context.driver.find_element(By.XPATH, "//button[@class='btn'][contains(.,'Regresar')]").click()
     context.driver.quit()
 
 @then('El usuario es redirigido a la página de inicio o home')
 def login_success(context):
-    assert "Secure Page page for Automation Testing Practice" in context.driver.title 
+    assert "P AAutomated Company Pass and User" in context.driver.title 
 
 @then('El usuario visualiza el mensaje de bienvenida')
-def login_success(context):
-    assert "Welcome to the Secure Area. When you are done click logout below." in context.driver.page_source
+def show_welcome_message(context):
+    context.driver.find_element(By.XPATH, "//p[@class='name-menu'][contains(.,'Hola, User')]")
+    time.sleep(2)
+    
 
 @then('El usuario cierra sesión')
-def login_success(context):
-    context.driver.find_element(By.XPATH,"//i[@class='icon-2x icon-signout'][contains(.,'Logout')]").click()
+def user_logout(context):
+    context.driver.find_element(By.XPATH,"//img[contains(@class,'icon-close-s')]").click()
+    time.sleep(2)
     context.driver.quit()
